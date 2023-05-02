7817183734557bb710276c8ed5d1abf3ae70520e
Lukas Martini <hello@lutoma.org>
2023-05-02 18:50:05 +0200

    Initial Bootstrap setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: []
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "src/vars.scss";',
+        },
+      },
+    },
+
+  }
 });
